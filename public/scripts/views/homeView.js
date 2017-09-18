a6c89dcff8c1c7d88320a321f4e4bdaa190e55fe
Zach Taylor <ztaylor2@zagmail.gonzaga.edu>
2017-09-18 15:57:46 -0700

    functional hero img

---
+++ public/scripts/views/homeView.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var imageArray = ['frog.jpg', 'gnome.jpg', 'goat.jpg', 'lake.jpg', 'pond.jpg', 'raft.jpg', 'rock.jpg', 'sean.jpg']
+
+for(let i = 0; i < imageArray.length; i++) {
+  setTimeout(function() {
+    $('.heroImage').attr('src',`img/${imageArray[i]}`);
+  }, i * 5000);
+}
