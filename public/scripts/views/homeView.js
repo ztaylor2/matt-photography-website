3e13fe7523aae39dd1b3ade95d6c9788a521c6b6
Zach Taylor <ztaylor2@zagmail.gonzaga.edu>
2017-09-20 12:14:47 -0700

    commiting before changing up slides code

--- public/scripts/views/homeView.js
+++ public/scripts/views/homeView.js
@@ -1,9 +1,10 @@
 'use strict';
 
-var imageArray = ['frog.jpg', 'gnome.jpg', 'goat.jpg', 'lake.jpg', 'pond.jpg', 'raft.jpg', 'rock.jpg', 'sean.jpg']
+var imageArray = ['frog.jpg', 'gnome.jpg', 'goat.jpg', 'lake.jpg', 'pond.jpg', 'raft.jpg', 'rock.jpg', 'sean.jpg'];
 
 for(let i = 0; i < imageArray.length; i++) {
   setTimeout(function() {
     $('.heroImage').attr('src',`img/${imageArray[i]}`);
-  }, i * 5000);
+  }, i * 4000);
+  console.log(i);
 }
